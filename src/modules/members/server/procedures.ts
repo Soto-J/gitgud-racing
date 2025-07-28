fb319472b1ac35a363a79b1c3f805c556569d40c
John Soto <soto.ja415@gmail.com>
2025-07-28 18:02:35 -0400

    feat: add pagination and search filters to Members page; integrate nuqs for query state management

--- src/modules/members/server/procedures.ts
+++ src/modules/members/server/procedures.ts
@@ -1,18 +1,66 @@
 import z from "zod";
 
-import { eq } from "drizzle-orm";
+import { count, desc, eq, like, and } from "drizzle-orm";
 
 import { db } from "@/db";
 import { user } from "@/db/schema";
 
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
+import {
+  DEFAULT_PAGE,
+  DEFAULT_PAGE_SIZE,
+  MAX_PAGE_SIZE,
+  MIN_PAGE_SIZE,
+} from "@/constants";
 
 export const membersRouter = createTRPCRouter({
-  getMany: protectedProcedure.query(async ({ ctx, input }) => {
-    const members = await db.select().from(user);
+  getMany: protectedProcedure
+    .input(
+      z.object({
+        page: z.number().default(DEFAULT_PAGE),
+        pageSize: z
+          .number()
+          .min(MIN_PAGE_SIZE)
+          .max(MAX_PAGE_SIZE)
+          .default(DEFAULT_PAGE_SIZE),
+        search: z.string().nullish(),
+        memberId: z.string().nullish(),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      const { memberId, page, pageSize, search } = input;
 
-    return members;
-  }),
+      const members = await db
+        .select()
+        .from(user)
+        .where(
+          and(
+            memberId ? eq(user.id, memberId) : undefined,
+            search ? like(user.name, `%${search}%`) : undefined,
+          ),
+        )
+        .orderBy(desc(user.createdAt), desc(user.id))
+        .limit(pageSize)
+        .offset((page - 1) * pageSize);
+
+      const [total] = await db
+        .select({ count: count() })
+        .from(user)
+        .where(
+          and(
+            memberId ? eq(user.id, memberId) : undefined,
+            search ? like(user.name, `%${search}%`) : undefined,
+          ),
+        );
+
+      const totalPages = Math.ceil(total.count / pageSize);
+
+      return {
+        members,
+        total: total.count,
+        totalPages,
+      };
+    }),
 
   getOne: protectedProcedure
     .input(z.object({ id: z.string() }))
