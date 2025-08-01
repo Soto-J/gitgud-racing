b27715bc8343ce8a2b756e4f1b7665fb6bec4bb2
John Soto <soto.ja415@gmail.com>
2025-07-31 21:05:12 -0400

    Testing iracing api

--- src/app/(dashboard)/(home)/page.tsx
+++ src/app/(dashboard)/(home)/page.tsx
@@ -4,11 +4,17 @@ import { headers } from "next/headers";
 import { auth } from "@/lib/auth";
 
 import { HomeView } from "@/modules/home/ui/views/home-view";
+import { getQueryClient, trpc } from "@/trpc/server";
 
 const DashboardPage = async () => {
   const session = await auth.api.getSession({ headers: await headers() });
 
   if (!session) redirect("/sign-in");
+
+  // const queryClient = getQueryClient();
+  // void queryClient.prefetchQuery(
+  //   trpc.iracing.testIracing.queryOptions({ userId: session.user.id }),
+  // );
 
   return <HomeView userId={session.user.id} />;
 };
