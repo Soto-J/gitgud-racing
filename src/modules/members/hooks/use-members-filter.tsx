42f9b90e80d8c0af52cc4e3e4238953e44c0bf51
John Soto <soto.ja415@gmail.com>
2025-08-21 12:30:24 -0400

    fix import typo

--- src/modules/members/hooks/use-members-filter.tsx
+++ src/modules/members/hooks/use-members-filter.tsx
@@ -1,6 +1,6 @@
-import { DEFAULT_PAGE } from "@/constants";
-
 import { parseAsInteger, parseAsString, useQueryStates } from "nuqs";
+
+import { DEFAULT_PAGE } from "@/modules/members/constants";
 
 // if input is empty clears URL to default ""
 // E.g. http://localhost:3000/agents?search=test
