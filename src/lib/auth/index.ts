9c3a2a60fe8286f3b16a4db98cb2c96f05ad5fd9
John Soto <soto.ja415@gmail.com>
2026-01-19 02:57:08 -0500

    Comment out getToken function for debugging purposes

--- src/lib/auth/index.ts
+++ src/lib/auth/index.ts
@@ -81,58 +81,59 @@ export const auth = betterAuth({
           redirectURI: `${env.NEXT_PUBLIC_APP_URL}/api/auth/callback/iracing`,
           authorizationUrl: "https://oauth.iracing.com/oauth2/authorize",
           tokenUrl: "https://oauth.iracing.com/oauth2/token",
+   
           scopes: ["iracing.auth", "iracing.profile"],
           authorizationUrlParams: { audience: "data-server", debug: "1" },
           pkce: true,
 
-          async getToken({ code, redirectURI, codeVerifier }) {
-            console.log("Testing");
-            if (!codeVerifier) {
-              throw new Error("Code verifier missing");
-            }
-            const response = await fetch(
-              "https://oauth.iracing.com/oauth2/token",
-              {
-                method: "POST",
-                headers: {
-                  "Content-Type": "application/x-www-form-urlencoded",
-                },
-                body: new URLSearchParams({
-                  grant_type: "authorization_code",
-                  code,
-                  redirect_uri: redirectURI,
-                  client_id: env.IRACING_CLIENT_ID,
-                  client_secret: maskIRacingSecret(
-                    env.IRACING_AUTH_SECRET,
-                    env.IRACING_CLIENT_ID,
-                  ),
-                  code_verifier: codeVerifier,
-                }),
-              },
-            );
+          // async getToken({ code, redirectURI, codeVerifier }) {
+          //   console.log("Testing");
+          //   if (!codeVerifier) {
+          //     throw new Error("Code verifier missing");
+          //   }
+          //   const response = await fetch(
+          //     "https://oauth.iracing.com/oauth2/token",
+          //     {
+          //       method: "POST",
+          //       headers: {
+          //         "Content-Type": "application/x-www-form-urlencoded",
+          //       },
+          //       body: new URLSearchParams({
+          //         grant_type: "authorization_code",
+          //         code,
+          //         redirect_uri: redirectURI,
+          //         client_id: env.IRACING_CLIENT_ID,
+          //         client_secret: maskIRacingSecret(
+          //           env.IRACING_AUTH_SECRET,
+          //           env.IRACING_CLIENT_ID,
+          //         ),
+          //         code_verifier: codeVerifier,
+          //       }),
+          //     },
+          //   );
 
-            if (!response.ok) {
-              const text = await response.text();
-              throw new Error(`Failed to exchange code for token: ${text}`);
-            }
+          //   if (!response.ok) {
+          //     const text = await response.text();
+          //     throw new Error(`Failed to exchange code for token: ${text}`);
+          //   }
 
-            const payload = await response.json();
+          //   const payload = await response.json();
 
-            console.log(payload);
-            const data = TokenRespnseSchema.parse(payload);
+          //   console.log(payload);
+          //   const data = TokenRespnseSchema.parse(payload);
 
-            return {
-              accessToken: data.access_token,
-              refreshToken: data.refresh_token,
-              accessTokenExpiresAt: new Date(
-                Date.now() + data.expires_in * 1000,
-              ),
-              refreshTokenExpiresAt: new Date(
-                Date.now() + data.refresh_token_expires_in * 1000,
-              ),
-              scope: data.scope,
-            };
-          },
+          //   return {
+          //     accessToken: data.access_token,
+          //     refreshToken: data.refresh_token,
+          //     accessTokenExpiresAt: new Date(
+          //       Date.now() + data.expires_in * 1000,
+          //     ),
+          //     refreshTokenExpiresAt: new Date(
+          //       Date.now() + data.refresh_token_expires_in * 1000,
+          //     ),
+          //     scope: data.scope,
+          //   };
+          // },
 
           async getUserInfo(tokens) {
             const initialResponse = await fetch(
